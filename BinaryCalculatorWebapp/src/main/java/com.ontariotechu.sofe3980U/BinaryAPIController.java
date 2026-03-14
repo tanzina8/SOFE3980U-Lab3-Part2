698ac58ffb001d03623781398e9363c93886fbaa
tanzina8 <168616206+tanzina8@users.noreply.github.com>
2026-03-14 05:03:20 -0400

    Add multiplication, AND, and OR operations

--- BinaryCalculatorWebapp/src/main/java/com.ontariotechu.sofe3980U/BinaryAPIController.java
+++ BinaryCalculatorWebapp/src/main/java/com.ontariotechu.sofe3980U/BinaryAPIController.java
@@ -27,4 +27,28 @@ public class BinaryAPIController {
 		// http://localhost:8080/add?operand1=111&operand2=1010
 	}
 
+	@GetMapping("/multiply")
+	public String multiply(@RequestParam(name="operand1", required=false, defaultValue="") String operand1,
+                       @RequestParam(name="operand2", required=false, defaultValue="") String operand2) {
+    	Binary number1 = new Binary(operand1);
+    	Binary number2 = new Binary(operand2);
+    	return Binary.multiply(number1, number2).getValue();
+	}
+
+	@GetMapping("/and")
+	public String and(@RequestParam(name="operand1", required=false, defaultValue="") String operand1,
+                  @RequestParam(name="operand2", required=false, defaultValue="") String operand2) {
+    	Binary number1 = new Binary(operand1);
+    	Binary number2 = new Binary(operand2);
+    	return Binary.and(number1, number2).getValue();
+	}
+
+	@GetMapping("/or")
+	public String or(@RequestParam(name="operand1", required=false, defaultValue="") String operand1,
+                 @RequestParam(name="operand2", required=false, defaultValue="") String operand2) {
+    	Binary number1 = new Binary(operand1);
+    	Binary number2 = new Binary(operand2);
+    	return Binary.or(number1, number2).getValue();
+	}
+
 }
